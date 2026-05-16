2b96874577e67065e45f27aaca801fa8dc51c201
xyleneSVG <dhavinf.a@gmail.com>
2026-05-16 17:42:40 +0700

    fix: root frontend

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from "express"
+import path from "path"
+import { fileURLToPath } from "url"
 import apiApp from "./api/index.js"
 
 const app = express()
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.static(path.join(__dirname, "public")))
+
 app.use(apiApp)
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+})
 
 export default app
